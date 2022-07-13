44db3fce7af69132f43e0a783931491b067c19dc
Shamili05 <104269679+Shamili05@users.noreply.github.com>
2022-07-13 16:00:15 +0530

    Add files via upload

---
+++ quiz_app/quiz-web-application-django/Quiz/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+
+    
+    path('', views.home, name='home'),
+    path('addQuestion/', views.addQuestion,name='addQuestion'),
+    path('login/', views.loginPage,name='login'),
+    path('logout/',views.logoutPage,name='logout'),
+    path('register/', views.registerPage,name='register'),
+]
+  
